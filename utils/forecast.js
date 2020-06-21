41ea04a300a33d3e4552382161635f5d8e0def5f
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-06-20 19:47:10 -0700

    added text to print forecast

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,7 +3,7 @@ const search = require('./search');
 
 
 const forecast = (forecastData) => {
-    console.log(forecastData)
+    console.log(`In ${forecastData.location.name}, it is ${forecastData.current.weather_descriptions[0]}. The temperature is ${(((forecastData.current.temperature)*(9/5))+32)} degrees Fahrenheit. There is a ${forecastData.current.precip} percent change of rain. It feels like it is ${(((forecastData.current.feelslike)*(9/5))+32)} degrees Fahrenheit.`);   
 };
 
 
